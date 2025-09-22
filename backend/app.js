9f14e319cf1e38d2e21089b6dc56582a2a268e59
ducdz1m2 <93931868+ducdz1m2@users.noreply.github.com>
2025-09-22 10:55:54 +0700

    them trang xu ly loi

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,24 @@
 const express = require("express")
 const cors = require("cors")
 const booksRouter = require("./app/routes/book.route")
+const ApiError = require("./app/api-error")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/books", booksRouter)
-
-app.get("/", (req, res) => {
-    res.json({ message: "welcome"})
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Không tìm thấy tài nguyên!"))
 })
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Lỗi bên trong server!"
+    })
+})
+app.get("/", (req, res) => {
+    res.json({ message: "Chào mừng đến với hệ thống mượn sách!"})
+})
+
 module.exports = app;
 
