f6217ed0df0f7d9fa1ae418333d5b9f4ba1e0c78
ducdz1m2 <93931868+ducdz1m2@users.noreply.github.com>
2025-09-23 22:33:21 +0700

    ma hoa mat khau staff

--- backend/app/services/staff.service.js
+++ backend/app/services/staff.service.js
@@ -1,68 +1,71 @@
 const { ObjectId } = require("mongodb");
+const bcrypt = require("bcrypt");
 
 class StaffService {
-    constructor(client) {
-        this.Staff = client.db().collection("staff");
-    }
+  constructor(client) {
+    this.Staff = client.db().collection("staff");
+  }
 
-    extractStaffData(payload) {
-        const staff = {
-            hoTenNV: payload.hoTenNV,
-            password: payload.password, // nên hash trước khi lưu
-            chucVu: payload.chucVu,
-            diaChi: payload.diaChi,
-            soDienThoai: payload.soDienThoai,
-        };
-        Object.keys(staff).forEach(
-            (key) => staff[key] === undefined && delete staff[key]
-        );
-        return staff;
-    }
+  extractStaffData(payload) {
+    const staff = {
+      hoTenNV: payload.hoTenNV,
+      password: payload.password
+        ? bcrypt.hashSync(payload.password, 10) // hash trước khi lưu
+        : undefined,
+      chucVu: payload.chucVu,
+      diaChi: payload.diaChi,
+      soDienThoai: payload.soDienThoai,
+    };
+    Object.keys(staff).forEach(
+      (key) => staff[key] === undefined && delete staff[key]
+    );
+    return staff;
+  }
 
-    async create(payload) {
-        const staff = this.extractStaffData(payload);
-        const result = await this.Staff.findOneAndUpdate(
-            { hoTenNV: staff.hoTenNV, soDienThoai: staff.soDienThoai }, // tránh trùng
-            { $set: staff },
-            { returnDocument: "after", upsert: true }
-        );
-        return result.value;
-    }
+  async create(payload) {
+    const staff = this.extractStaffData(payload);
+    const result = await this.Staff.findOneAndUpdate(
+      { hoTenNV: staff.hoTenNV, soDienThoai: staff.soDienThoai }, // tránh trùng
+      { $set: staff },
+      { returnDocument: "after", upsert: true }
+    );
+    return result.value;
+  }
 
-    async find(filter) {
-        const cursor = await this.Staff.find(filter);
-        return await cursor.toArray();
-    }
+  async find(filter) {
+    const cursor = await this.Staff.find(filter);
+    return await cursor.toArray();
+  }
 
-    async findById(id) {
-        if (!ObjectId.isValid(id)) return null;
-        return await this.Staff.findOne({ _id: new ObjectId(id) });
-    }
+  async findById(id) {
+    if (!ObjectId.isValid(id)) return null;
+    return await this.Staff.findOne({ _id: new ObjectId(id) });
+  }
 
-    async update(id, payload) {
-        if (!ObjectId.isValid(id)) return null;
-        const filter = { _id: new ObjectId(id) };
-        const update = this.extractStaffData(payload);
-        const result = await this.Staff.findOneAndUpdate(
-            filter,
-            { $set: update },
-            { returnDocument: "after" }
-        );
-        return result.value;
-    }
+  async update(id, payload) {
+    if (!ObjectId.isValid(id)) return null;
+    const filter = { _id: new ObjectId(id) };
+    const update = this.extractStaffData(payload);
+    const result = await this.Staff.findOneAndUpdate(
+      filter,
+      { $set: update },
+      { returnDocument: "after" }
+    );
+    return result.value;
+  }
 
-    async delete(id) {
-        if (!ObjectId.isValid(id)) return null;
-        const result = await this.Staff.findOneAndDelete({
-            _id: new ObjectId(id),
-        });
-        return result.value;
-    }
+  async delete(id) {
+    if (!ObjectId.isValid(id)) return null;
+    const result = await this.Staff.findOneAndDelete({
+      _id: new ObjectId(id),
+    });
+    return result.value;
+  }
 
-    async deleteAll() {
-        const result = await this.Staff.deleteMany({});
-        return result.deletedCount;
-    }
+  async deleteAll() {
+    const result = await this.Staff.deleteMany({});
+    return result.deletedCount;
+  }
 }
 
 module.exports = StaffService;
